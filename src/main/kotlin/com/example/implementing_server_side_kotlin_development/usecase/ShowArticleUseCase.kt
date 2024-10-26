b5264842c0a9571d14d014012ac5ba774bf4e353
ISAWASHUN <syun20000616@icloud.com>
2024-10-26 22:52:17 +0900

    UseCase層の実装

--- src/main/kotlin/com/example/implementing_server_side_kotlin_development/usecase/ShowArticleUseCase.kt
+++ src/main/kotlin/com/example/implementing_server_side_kotlin_development/usecase/ShowArticleUseCase.kt
@@ -9,4 +9,28 @@ interface ShowArticleUseCase {
 }
 
 @Service
-class ShowArticleUseCaseImpl: ShowArticleUseCase
+class ShowArticleUseCaseImpl(
+  val articleRepository: ArticleRepository
+): ShowArticleUseCase {
+  override fun execute(slug: String) : Either<ShowArticleUseCase.Error, CreatedArticle> {
+    /**
+     * slug の検証
+     *
+     * 不正な slug だった場合、早期 return
+     */
+    val validatedSlug = Slug.new(slug).getOrElse {return ShowArticleUseCase.Error.ValidationErrors(it.all).left()}
+
+    /**
+     * 記事の取得
+     *
+     * 取得失敗した場合、早期 return
+     */
+    val createdArticle = articleRepository.findBySlug(validatedSlug).getOrElse { 
+      return when (it) {
+        is ArticleRepository.FindBySlugError.NotFound -> ShowArticleUseCase.Error.NotFoundArticle(it.slug.value).left()
+      }
+     }
+
+    return createdArticle.right()
+  }
+}
