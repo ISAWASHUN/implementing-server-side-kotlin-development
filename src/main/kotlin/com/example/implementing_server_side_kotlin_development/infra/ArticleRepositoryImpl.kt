5537df2c59a5e311200f429151c1daff1ba62170
ISAWASHUN <syun20000616@icloud.com>
2024-10-26 23:06:10 +0900

    インフラ層の実装

--- src/main/kotlin/com/example/implementing_server_side_kotlin_development/infra/ArticleRepositoryImpl.kt
+++ src/main/kotlin/com/example/implementing_server_side_kotlin_development/infra/ArticleRepositoryImpl.kt
@@ -3,4 +3,45 @@
  *
  */
 @Repository
-class ArticleRepositoryImpl : ArticleRepository
+class ArticleRepositoryImpl(
+  val namedParameterJdbcTemplate: NamedParameterJdbcTemplate
+  ): ArticleRepository {
+  /**
+   * slug から作成済記事を取得
+   *
+   * @param slug
+   * @return
+   */
+  override fun findBySlug(slug: Slug): Either<FindBySlugError, CreatedArticle> {
+    override fun findBySlug(slug: Slug): Either<FindBySlugError, CreatedArticle> {
+      val sql = """
+        SELECT
+          articles.slug,
+          articles.title,
+          articles.description,
+          articles.body
+        FROM 
+          articles
+        WHERE 
+          articles.slug = :slug
+      """.trimIndent()
+      val articleMapList = namedParameterJdbcTemplate.queryForList(sql, MapSqlParameterSource().addValue("slug", slug.value))
+
+        /**
+         * DB から作成済記事が見つからなかった場合、早期 return
+         */
+        if (articleMapList.isEmpty()) {
+            return ArticleRepository.FindBySlugError.NotFound(slug = slug).left()
+        }
+
+        val articleMap = articleMapList.first()
+
+        return CreatedArticle.newWithoutValidation(
+            slug = Slug.newWithoutValidation(articleMap["slug"].toString()),
+            title = Title.newWithoutValidation(articleMap["title"].toString()),
+            description = Description.newWithoutValidation(articleMap["description"].toString()),
+            body = Body.newWithoutValidation(articleMap["body"].toString()),
+        ).right()
+    }
+  }
+}
